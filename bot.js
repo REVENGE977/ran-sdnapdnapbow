69f1894cdde78a48af8c8bcd3cc529cdea5fc9da
ShadowX <32039975+ShadwX57@users.noreply.github.com>
2018-09-16 20:20:28 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,70 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[═════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
+
+ 
+console.log('Welcome')
+
+ 
+client.on("message", message => {
+  if(!message.member.hasPermission('ADMINISTRATOR')) return;
+  
+ 
+  function discoRole() {
+    let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    roles.forEach((role) => {
+      let theRole = message.guild.roles.find("name", role);
+      if(!theRole) return;
+      theRole.edit({color: random}).catch(e => {
+        return message.channel.send(":x: **Error:** The role you specified in the `config.json` is either not a role on this server, or his a role higher than the highest role that I have.");
+      });
+    });
+  }
+ 
+ if(message.content.startsWith(prefix + "startrainbow")) {
+    if(!message.member.hasPermission('ADMINISTRATOR')) return
+    setInterval(() => { discoRole(); }, config.ms);
+    message.channel.send("```css\nRainbowing...```");
+    message.channel.send("Make Sure You Make Role With **Rainbow** Name");
+  } else {
+  }
+ 
+if(message.content.startsWith(prefix + "stoprainbow")) {
+  message.channel.send("I've stopped rainbowing.");
+  setTimeout(() => { console.log(process.exit(0)); }, 300);
+} else {
+  }
+}
+ 
+)
+const config = require('./config.json');
+const prefix = config.prefix;
+const roles = config.roleToDisco;
+
+
+client.login(config.token)
